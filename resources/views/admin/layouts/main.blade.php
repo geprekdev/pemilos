0aacbe0b93eefbccc07aa475227fa99a26cc0147
Aydin Pramasta <pramastaaydin@gmail.com>
2022-09-10 16:26:13 +0700

    remove unnecessary link & script tag

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -14,9 +14,6 @@
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
   <script src="{{ asset('js/init-alpine.js') }}"></script>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
-
   @yield('assets')
 </head>
 
