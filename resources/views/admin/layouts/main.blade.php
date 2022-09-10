a27bd320d7bdcdbf35e3eed0323df60cb0eac980
Aydin Pramasta <pramastaaydin@gmail.com>
2022-09-10 12:24:28 +0700

    add & configure tailwindcss

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html :class="{ 'theme-dark': dark }" x-data="data()" lang="id">
+<html class="dark" x-data="data()" lang="id">
 
 <head>
   <meta charset="UTF-8" />
@@ -9,7 +9,7 @@
 
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
 
-  <link rel="stylesheet" href="{{ asset('css/tailwind.output.css') }}" />
+  @vite('resources/css/app.css')
 
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
   <script src="{{ asset('js/init-alpine.js') }}"></script>
@@ -330,24 +330,6 @@
             </svg>
           </button>
           <ul class="flex items-center flex-shrink-0 space-x-6" style="margin-left: auto">
-            <!-- Theme toggler -->
-            <li class="flex">
-              <button class="rounded-md focus:outline-none focus:shadow-outline-purple" @click="toggleTheme"
-                aria-label="Toggle color mode">
-                <template x-if="!dark">
-                  <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                  </svg>
-                </template>
-                <template x-if="dark">
-                  <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd"
-                      d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                      clip-rule="evenodd"></path>
-                  </svg>
-                </template>
-              </button>
-            </li>
             <li class="flex">
               <form action="{{ route('logout') }}" method="POST">
                 @csrf
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
