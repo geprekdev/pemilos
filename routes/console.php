598564ad61fcd2f1e1ae70b60b24a361c5b946b1
Aydin Pramasta <pramastaaydin@gmail.com>
2022-09-11 08:18:03 +0700

    add artisan import:user command

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,53 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('import:user {file}', function ($file) {
+    $path = '';
+    if (str_starts_with($file, '/')) {
+        $path = $file;
+    } else {
+        $path = getcwd() . '/' . $file;
+    };
+
+    if ($stream = fopen($path, 'r')) {
+        DB::transaction(function () use ($stream) {
+            $firstRow = true;
+            while ($data = fgetcsv($stream, 1000, ',')) {
+                if ($firstRow) {
+                    $firstRow = false;
+                    continue;
+                }
+
+                $role = null;
+                $class = null;
+                switch (strtoupper($data[2])) {
+                    case 'MURID':
+                        $role = User::STUDENT;
+                        $class = $data[3];
+                        break;
+                    case 'GURU':
+                        $role = User::TEACHER;
+                        break;
+                    case 'STAFF':
+                        $role = User::STAFF;
+                        break;
+                }
+
+                try {
+                    User::create([
+                        'name' => $data[0],
+                        'username' => $data[1],
+                        'role_id' => $role,
+                        'class' => $class ?? null,
+                        'password' => bcrypt(strtoupper($data[2]) . $data[1]),
+                    ]);
+                    $this->info($data[0]);
+                } catch (QueryException $exception) {
+                    $this->warn($exception->getMessage() . PHP_EOL);
+                }
+            }
+            fclose($stream);
+        });
+    }
+})->purpose('Seed User from CSV file');
