9627e8cbd16c4fd999d5397b8eed8765e3e35fe5
Aydin Pramasta <pramastaaydin@gmail.com>
2022-09-13 13:43:19 +0700

    add Candidate model, migration and factory

---
+++ database/factories/CandidateFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Candidate>
+ */
+class CandidateFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'label' => collect(['MPK', 'OSIS'])->random(),
+            'number' => fake()->numberBetween(1, 3),
+            'image' => Str::random(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Candidate;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -17,5 +18,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory(10)->create();
+
+        Candidate::factory(4)->create();
     }
 }
