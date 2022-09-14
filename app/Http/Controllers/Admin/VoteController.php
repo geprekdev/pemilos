ce408e1e183716cc2ae8e41e05895230ede98bc4
Aydin Pramasta <pramastaaydin@gmail.com>
2022-09-15 06:56:36 +0700

    add Vote navbar & permission

--- app/Http/Controllers/Admin/VoteController.php
+++ app/Http/Controllers/Admin/VoteController.php
@@ -10,6 +10,8 @@ class VoteController extends Controller
 {
     public function index()
     {
+        abort_if(!in_array(auth()->user()->role_id, [User::SUPER_ADMIN, User::ADMIN]), 404);
+
         $users = User::with('votes')->whereNotIn('role_id', [User::SUPER_ADMIN, User::ADMIN])->paginate(36);
 
         return view('admin.votes.index', compact('users'));
--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -76,6 +76,23 @@
               </a>
             </li>
           @endif
+
+          <li class="relative px-6 py-3">
+            @if (str_contains(request()->url(), route('admin.votes.index')))
+              <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                aria-hidden="true"></span>
+            @endif
+            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 {{ str_contains(request()->url(), route('admin.votes.index')) ? 'text-gray-800  dark:text-gray-100' : '' }}"
+              href="{{ route('admin.votes.index') }}">
+              <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round"
+                stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <path
+                  d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+              </svg>
+
+              <span class="ml-4">Suara</span>
+            </a>
+          </li>
         </ul>
       </div>
     </aside>
@@ -149,6 +166,23 @@
               </a>
             </li>
           @endif
+
+          <li class="relative px-6 py-3">
+            @if (str_contains(request()->url(), route('admin.votes.index')))
+              <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                aria-hidden="true"></span>
+            @endif
+            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 {{ str_contains(request()->url(), route('admin.votes.index')) ? 'text-gray-800  dark:text-gray-100' : '' }}"
+              href="{{ route('admin.votes.index') }}">
+              <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round"
+                stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <path
+                  d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+              </svg>
+
+              <span class="ml-4">Suara</span>
+            </a>
+          </li>
         </ul>
       </div>
     </aside>
