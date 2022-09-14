d46848032e4acbba2a46133c5edb32dce76850f9
Aydin Pramasta <pramastaaydin@gmail.com>
2022-09-14 17:57:20 +0700

    fix Candidate's factory to be more flexible

--- database/factories/CandidateFactory.php
+++ database/factories/CandidateFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 /**
@@ -17,11 +18,15 @@ class CandidateFactory extends Factory
      */
     public function definition()
     {
+        $file = 'candidates/' . Str::random() . '.jpg';
+
+        Storage::copy('candidate-no-pic.jpg', $file);
+
         return [
             'name' => fake()->name(),
             'label' => collect(['MPK', 'OSIS'])->random(),
             'number' => fake()->numberBetween(1, 3),
-            'image' => 'candidate-no-pic.jpg',
+            'image' => $file,
         ];
     }
 }
