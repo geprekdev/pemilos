0f9a7b822e3002f003e568705c9c4a6c10d792d3
Aydin Pramasta <pramastaaydin@gmail.com>
2022-10-09 07:20:21 +0700

    fix filter on admin votes

--- app/Http/Controllers/Admin/VoteController.php
+++ app/Http/Controllers/Admin/VoteController.php
@@ -14,10 +14,12 @@ class VoteController extends Controller
 
         $users = User::with('votes')->whereNotIn('role_id', [User::SUPER_ADMIN, User::ADMIN]);
 
-        $users->when(!empty($request->search), function ($query) use ($request) {
-            $query->where('name', 'LIKE', "%{$request->search}%")
-                ->orWhere('username', 'LIKE', "%{$request->search}%")
-                ->orWhere('class', 'LIKE', "%{$request->search}%");
+        $users->when(!empty($request->search), function ($users) use ($request) {
+            $users->where(function ($query) use ($request) {
+                $query->where('name', 'LIKE', "%{$request->search}%")
+                    ->orWhere('username', 'LIKE', "%{$request->search}%")
+                    ->orWhere('class', 'LIKE', "%{$request->search}%");
+            });
         });
 
         $users = $users->orderBy('class')->orderBy('name')->paginate(36);
